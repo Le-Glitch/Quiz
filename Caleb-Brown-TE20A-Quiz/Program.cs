be4407a42b6a728d546f998c3e256abc3e839268
Glitch <caleb.samuel.brown@gmail.com>
2021-09-29 13:11:48 +0200

    initial commit

---
+++ Caleb-Brown-TE20A-Quiz/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Caleb_Brown_TE20A_Quiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
